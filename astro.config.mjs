7e3a8b06f9b5f9e0ba1cc83421672fc8af2043b2
Matt Yang <43834543+datatango@users.noreply.github.com>
2026-08-14 09:34:50 -0500

    inline the github icon and patch dependency vulnerabilities

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,11 @@ import rehypeExternalLinks from "rehype-external-links";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
-import icon from "astro-icon";
 
 export default defineConfig({
   site: "https://www.matthewyang.io",
   base: "/",
-  integrations: [mdx(), sitemap(), react(), icon()],
+  integrations: [mdx(), sitemap(), react()],
   build: {
     inlineStylesheets: "always",
   },
